c7611502ed8ea953699d3fd89408ce91c05b4981
yuerua <yun16cat@gmail.com>
2022-06-01 23:57:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Exgender
+
+## Chrome extension to exchange gender pronounces
+
+> “There is no gender identity behind the expressions of gender.”
+
+### To use
+
+- Enable developer mode in Chrome extensions
+- Load unpacked extension
+
+### Please help!
+
+* Word list to be extended
+* How to capture ajax request in the webpage and modify the new contents? 
---
+++ content-script.js
@@ -0,0 +1,61 @@
+var elements = document.getElementsByTagName('*');
+
+var mapObj_ = {
+    男:"女",
+    他:"她",
+    先生:"小姐",
+    老婆:"老公",
+    姐:"哥",
+    妹:"弟",
+    妻子:"丈夫",
+    爸:"妈",
+    姨:"叔"};
+
+
+var mapObj = {};
+for(var key in mapObj_){
+    mapObj[key] = mapObj_[key];
+    mapObj[mapObj_[key]] = key;
+}
+
+function replace_text(){
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+
+            if (node.nodeType === Node.TEXT_NODE) {
+                var text = node.nodeValue;
+                var re = new RegExp(Object.keys(mapObj).join("|"), "gi"); 
+                var replacedText = text.replace(re, function(matched){  
+                    return '『' + mapObj[matched] + '』';
+                    
+                });
+                // var replacedText = mapObj.reduce((f, s)  => `${f}`.replace(`{${i}}`, s), text)
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function()
+    {
+       replace_text();
+    });
+
+    // var observer = new MutationObserver(function(e) {
+    //     replace_text();
+    //   });
+      
+    // observer.observe(document.getElementById('*'), {
+    // attributes: true,
+    // characterData: true,
+    // childList: true,
+    // subtree: true
+    // });
